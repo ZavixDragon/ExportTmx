77be11d5e0bd74ae4b0bf376b6e207d821b5c63c
noah reinagel <noah.r9@gmail.com>
2017-08-24 22:15:38 -0700

    Initial Commit

---
+++ ExportTmx/ExportTmx/Gold.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ExportTmx
+{
+    public class Gold
+    {
+        public Gold(string tmxPath, string destinationPath)
+        {
+            var doc = XDocument.Load(tmxPath);
+            var map = doc.Element("map");
+            var tmxDirectory = Path.GetDirectoryName(tmxPath);
+            var tilesets = map.Elements("tileset");
+            foreach (var tileset in tilesets.Where(x => x.Attribute("source") != null))
+            {
+                var tsxRelativePath = tileset.Attribute("source").Value;
+                var tsxDirectory = Path.GetDirectoryName(tsxRelativePath);
+                var tsxPath = Path.Combine(tmxDirectory, tsxRelativePath);
+                var tsxDoc = XDocument.Load(tsxPath);
+                var tsxTileset = tsxDoc.Element("tileset");
+                var tsxImage = tsxTileset.Element("image");
+                var tsxImageSource = tsxImage.Attribute("source").Value;
+                var imagePath = Path.Combine(tsxDirectory, tsxImageSource);
+                tsxImage.SetAttributeValue("source", imagePath);
+                tileset.ReplaceWith(tsxTileset);
+            }
+            foreach (var tileset in tilesets)
+            {
+                var tilesetImage = tileset.Element("image");
+                var imageSource = tilesetImage.Attribute("source");
+                var imagePath = Path.Combine(tmxDirectory, imageSource.Value);
+                var imageName = Path.GetFileName(imageSource.Value);
+                var destPath = Path.Combine(destinationPath, imageName);
+                File.Move(imagePath, destPath);
+                imageSource.SetValue(imageName);
+            }
+            var fullDest = Path.Combine(destinationPath, Path.GetFileName(tmxPath));
+            doc.Save(fullDest);
+        }
+    }
+}
---
+++ ExportTmx/ExportTmx/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExportTmx
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Gold(args[0], args[1]);
+        }
+    }
+}
